3756cbbcfa532ee03e3bf9b8061a30534bd1d8cf
NADJILEM Bégoto <jnadjilembegoto@gmail.com>
2025-04-05 20:40:13 +0100

    Update Fonctionnalities.py

--- Indabax_cm_Storytellers/Pages_utiles/Fonctionnalities.py
+++ Indabax_cm_Storytellers/Pages_utiles/Fonctionnalities.py
@@ -3,7 +3,37 @@ import streamlit as st
 
 
 def fonctionnalities_load():
-    st.title("*** Explorer les diverses fonctionnalités de votre dashboard ")
+    st.title(" Description détaillée des fonctionnalités du dashboard ")
+    
+    st.markdown("""
+    ### 📍 **Cartographie des donneurs**  
+    Répartition géographique interactive des donneurs pour identifier les zones à forte et faible concentration.  
+
+    ### 🏥 **Analyse des conditions de santé**  
+    Visualisation des critères d'éligibilité au don de sang pour mieux informer les donneurs potentiels.  
+
+    ### 🔬 **Profilage des donneurs**  
+    Clustering basé sur des données démographiques pour segmenter les donneurs et adapter les campagnes.  
+
+    ### 📊 **Efficacité des campagnes**  
+    Analyse des tendances et facteurs influents pour mesurer l’impact des campagnes de sensibilisation.  
+
+    ### 🔄 **Fidélisation des donneurs**  
+    Étude de la récurrence des dons et des facteurs incitant les donneurs à revenir régulièrement.  
+
+    ### 🧠 **Analyse de sentiment**  
+    Classification des retours des donneurs pour améliorer leur expérience et la communication.  
+
+    ### 🤖 **Modèle de prédiction**  
+    Utilisation d’un modèle de machine learning pour évaluer l’éligibilité d’un donneur potentiel.  
+
+    ### 🧑‍💻 **Chat Blood Assistant Analyst**  
+    Assistant interactif pour une compréhension approfondie des notions et des termes liés au don de sang.  
+
+    ### ℹ️ **About Us**  
+    Présentation de l’équipe Data Storytellers Team et des motivations derrière ce projet.  
+    """)
+    
 
     
 
